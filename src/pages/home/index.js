716604d155b97b12925bf0abaa2439842344bcb5
SergioDevSousa <oxentesergiosousa76@outlook.com>
2025-02-15 22:16:07 -0300

    update input

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,21 @@
-
+import { Header } from "../../components/Header";
+import background from "../../assets/background.png";
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <div className='conteudo'>
+        <img src={background} className='background' alt='background app'/>
+      <div className='info'>
+        <input name="usuario" placeholder="@username"/>
+        <button>Buscas</button>
+      </div>
       
+      </div>
     </div>
   );
-}
+};
 
 export default App;
