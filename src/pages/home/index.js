e0c17b90e7775726543fa8a55c7ae9504994002f
SergioDevSousa <oxentesergiosousa76@outlook.com>
2025-02-24 15:50:02 -0300

    final update

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,38 +1,70 @@
-import { useState } from 'react'
+import {useState} from 'react';
 import { Header } from "../../components/Header";
-import { Input } from "../../components/Input";
 import background from "../../assets/background.png";
 import ItemList from "../../components/ItemList";
 import './styles.css';
 
 function App() {
   const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    if (newUser.name) {
+      const { avatar_url, name, bio, login } = newUser;
+      setCurrentUser({ avatar_url, name, bio, login });
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json();
+
+      if (newRepos.length) {
+        setRepos(newRepos);
+      }
+    }
+  };
+
   return (
     <div className="App">
       <Header />
-      <div className='conteudo'>
-        <img src={background} className='background' alt='background app'/>
-        <div className='info'>
-      <Input />
-          <div className='perfil'>
-            <img src="https://avatars.githubusercontent.com/u/166451865?s=400&u=d2746a18715250730950244bec7d1a734f7be404&v=4" className="profile" alt="avatar"/>
-            <div>
-              <h3>Sérgio Sousa</h3>
-              <span>Descrição</span>
-              <p>Descrição</p>
-            </div>
-          </div>
-          <hr/>
+      <div className="conteudo">
+        <img src={background} className="background" alt="background app" />
+        <div className="info">
           <div>
-            <h4 className="repositorios" >Repositórios</h4>
-            <ItemList title='Nome do Repositório' description='Descrição do Repositório' />
-            <ItemList title='Nome do Repositório' description='Descrição do Repositório' />
-            <ItemList title='Nome do Repositório' description='Descrição do Repositório' />
+            <input
+              name="usuario"
+              value={user}
+              onChange={(event) => setUser(event.target.value)}
+              placeholder="@username"
+            />
+            <button onClick={handleGetData}>Buscar</button>
           </div>
+          {currentUser?.name ? (
+            <>
+                <div className="perfil">
+                  <img src={currentUser.avatar_url} className="profile" alt="avatar" />
+                  <div>
+                    <h3>{currentUser.name}</h3>
+                    <span>@{currentUser.login}</span>
+                    <p>{currentUser.bio}</p>
+                  </div>
+                </div>
+                <hr />
+            </>
+          ): null}
+          {repos?.length ? (
+              <div>
+                <h4 className="repositorios">Repositórios</h4>
+                  {repos.map(repo => (
+                    <ItemList title={repo.name} description={repo.description} />))} 
+              </div>
+          ) : null}
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default App;
